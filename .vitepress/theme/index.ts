d91a390a95a227f3ec4a15fd86eddc48304e92d2
KCSShj-ge <houjun-ge@kcss.com.cn>
2024-07-23 09:50:34 +0800

    add SpeedInsights function

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,11 +2,13 @@ import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 // @ts-ignore
 import comment from "../components/gitalk.vue";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx)
     ctx.app.component("gitalk", comment)
+    ctx.app.use(SpeedInsights)
   },
 };
